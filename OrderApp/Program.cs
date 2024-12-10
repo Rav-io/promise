b054e97ce4f07d895dcf6a3eee299e3da128c113
Rav-io <olejniczak.rafal99@gmail.com>
2024-12-10 12:29:16 +0100

    cli

--- OrderApp/Program.cs
+++ OrderApp/Program.cs
@@ -14,6 +14,39 @@ namespace OrderApp
                 new Product("Monitor", 1000),
                 new Product("Kaczka debuggująca", 66)
             };
+
+            while (true)
+            {
+                Console.WriteLine("\n---- Menu ----");
+                Console.WriteLine("1. Dodaj produkt");
+                Console.WriteLine("2. Usuń produkt");
+                Console.WriteLine("3. Wyświetl wartość zamówienia");
+                Console.WriteLine("4. Wyjście");
+                Console.Write("Wybierz opcję: ");
+
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("AddProducts");
+                        break;
+                    case "2":
+                        Console.WriteLine("remove");
+                        break;
+                    case "3":
+                        Console.WriteLine("display");
+                        break;
+                    case "4":
+                        Console.WriteLine("Dziękujemy za skorzystanie z aplikacji!");
+                        return;
+                    default:
+                        Console.WriteLine("Nieprawidłowa opcja. Spróbuj ponownie.");
+                        break;
+                }
+            }
+
         }
+
     }
 }
--- OrderApp/obj/Debug/net8.0/OrderApp.AssemblyInfo.cs
+++ OrderApp/obj/Debug/net8.0/OrderApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d5e711408042e36f5a87f680de54cc667e09f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
